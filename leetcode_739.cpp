186cd9ea90e9081d8f5fa77c240bede9b3476c9d
vishus0205 <vishus0205@gmail.com>
2025-11-01 16:04:01 +0530

    Update leetcode_739.cpp

--- leetcode_739.cpp
+++ leetcode_739.cpp
@@ -3,19 +3,20 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int>answer(n,0);
-        stack<int> st;
+        int n = temperatures.size(); //days for getting a warmer day
+        vector<int>answer(n,0); //initializes all the array values with 0 considering that there is no warmer day in future
+        stack<int> st;//this stores indexes of the days 
         for(int i = 0; i<n;i++){
-            while(!st.empty() && temperatures[i]>temperatures[st.top()]){
-                int prevIndex = st.top();
-                answer[prevIndex] = i-prevIndex;
-                st.pop();
+            while(!st.empty() && temperatures[i]>temperatures[st.top()]){ //checks that stack is not empty and current days temperature is warmer
+                int prevIndex = st.top();//stores the index of previous day which satisfies the while condition
+                answer[prevIndex] = i-prevIndex;//this shows the difference of the warmer day and the initail day,means no. of days  
+                st.pop();//removes the index from the stacks we checked and found answer
                 
             }
-            st.push(i);
+            st.push(i);//this pushes the index into stack because while condtion not satisfied means warmer day not found 
         }
-        return answer;
+        return answer; //gives the final array of the days 
         
     }
+
 };
