8e8fb491678225f01c22906d61d392b53e63d1da
vishus0205 <vishus0205@gmail.com>
2025-11-01 15:23:57 +0530

    Update leetcode_121.cpp
    Added comments to explain the logic of the maxProfit function.

--- leetcode_121.cpp
+++ leetcode_121.cpp
@@ -1,16 +1,19 @@
+//Q121.Best Time to Buy and Sell Stock
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minprice = INT_MAX;
-        int maxprofit = 0;
+        int minprice = INT_MAX; //assuming minimum price is highest price
+        int maxprofit = 0; //starting with 0 profit 
 
-        for(int price : prices){
-            if(price < minprice){
-                minprice = price;
-            }else if(price - minprice > maxprofit){
-                maxprofit = price - minprice;
+        for(int price : prices){  //for going through each days price in the array
+            if(price < minprice){  //if this days price is lower than past days price
+                minprice = price;  //this price becomes minprice till now
+            }else if(price - minprice > maxprofit){  //else we check that, is profit today higher than last days 
+                maxprofit = price - minprice; //calculation of profit 
             }
         }
-        return maxprofit;
+        return maxprofit; //giving highest profit return 
     }
+
 };
